bbd8582c7b9de78bc11700510de045cb63205d04
computer3314 <53067088+computer3314@users.noreply.github.com>
2022-10-24 01:17:35 +0800

    Update models.py

--- camera/models.py
+++ camera/models.py
@@ -10,12 +10,13 @@ class Camera(models.Model):
     class Meta:
         verbose_name = '攝影機'
         verbose_name_plural = '攝影機'
-    camera_id = models.TextField('網路攝影機', max_length=100,default='0')
+    camera_id = models.TextField('攝影機編號', max_length=100,blank=True,unique=True)
+    camera_url = models.TextField('攝影機位置', max_length=100,default='0',blank=True)
     title = models.CharField('標題', max_length=20,default='test')
     # Methods
     def camera_api(self):
         """Returns the URL to access a particular instance of MyModelName."""
-        return int(self.camera_id)
+        return int(self.camera_url)
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
