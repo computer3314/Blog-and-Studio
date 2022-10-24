7f6105ae5cc4e3f37f3e5bfb39376252b0ffc47e
7162 陳昜睿 happychen <11105002@pershing.com.tw>
2022-10-24 18:11:29 +0800

    20221024

--- camera/cameras.py
+++ camera/cameras.py
@@ -22,55 +22,85 @@ class CameraException(Exception):
     def __str__(self):
         return self.message
 
-
 class BaseCamera:
     #相機對象
     cam = None
     # 後台取幀數的線程
     thread = None
     queue = None
-    # 相機modal
-    camera_model = None
-    width = 1920
-    height = 1080
+    #相機名稱
+    title =None
+    #解析度設定
+    width = 1280
+    height = 720
      # 輸出目錄
-    outputFolder = "static/my_output"
-    #計算幀數幀
+    outputBaseFolder = "static/my_output/"
+    outputFolder = None
+    #計算幀數
     counter = 0
-    fps = 0
+    fps = 10
     start_time=time.time()
     #計算目前秒數
     min = '2022-01-01 8:01:01'
      #靈敏度
     fast=4
-   #檔案超過多久刪除
+   #檔案超過多久刪除(天數)
     Dday = 1
+    #移動偵測通知時間(秒數)
+    moveNotice = 60
+    #是否開啟寄信
+    mail_check=False
+    #是否移動拍照
+    scan_check=True
+    #是否移動拍照
+    isOpened=True
     # 相機基礎類
     def __init__(self, camera_model: Camera,queue_image:queue):
         
         self.cam = cv2.VideoCapture(camera_model.camera_api(),cv2.CAP_DSHOW)
-         # 自動建立目錄
+        self.set_defalut(camera_model)
+        self.outputFolder=self.outputBaseFolder+str(self.title)       
+        # 自動建立目錄
         if not os.path.exists(self.outputFolder):
          os.makedirs(self.outputFolder)
         else:
          self._removeold()
-        if self.cam.isOpened():
-            self.fps=self.cam.get(cv2.CAP_PROP_FPS)
+        if self.cam is None:
+              # 打開失敗
+            print("訪問失敗")
+            raise CameraException("訪問失敗")
+        if self.cam.isOpened() and self.isOpened:
             self.queue=queue_image
             # 相機打開成功
             self.thread = threading.Thread(target=self._thread, daemon=True)
             self.thread.start()
         else:
-            # 打開是失敗
-            self.cam = None
+            # 打開失敗
+            print("訪問失敗")
             raise CameraException("訪問失敗")
-   
+    def set_defalut(self,camera_model: Camera):
+        #讀取基礎設定
+        self.width=camera_model.width
+        self.height=camera_model.heigth
+        self.fast=camera_model.fast
+        self.Dday=camera_model.dday
+        self.moveNotice=camera_model.moveNotice
+        self.mail_check=camera_model.mailCheck
+        self.scan_check=camera_model.scancheck
+        self.fps=camera_model.fps
+        self.isOpened=camera_model.isOpened
+        self.title=camera_model.title
+        if self.cam is not None:
+            self.cam.set(cv2.CAP_PROP_FPS,self.fps) 
+        if self.isOpened is False:
+            self.cam=None
+        
     def _thread(self):
         """
       opencv讀取時會將信息存儲到緩存區裡，處理速度小於緩存區速度，會導致資源積累
         """
         # 線程一直讀取影片流，將最新的視頻流存在隊列中
-        while self.cam.isOpened():
+        while self.cam is not None and self.cam.isOpened():
             ret, img = self.cam.read()
             if not ret or img is None:
                 # 讀取失敗
@@ -85,8 +115,6 @@ class BaseCamera:
                 else:
                     # 插入最後面
                     self.queue.put(img)
-    def __del__(self):
-        self.cam.release()
     # 直接讀取圖片
     def read(self):
         """
@@ -94,26 +122,44 @@ class BaseCamera:
         可能為空，需做判空處理
         """
         return self.queue_image.get()
-    def send_mail(self,img):
-       # 電子郵件內容樣板
-            localtime = time.localtime()
-            check=self.timeSecond()
-            if check is not None:           
+    def move_notice(self,img):
+       # 移動偵測
+            check=self.timeSecond(self.moveNotice)
+            if check is not None:
+                localtime = time.localtime()
                 result1 = time.strftime("%Y%m%d%I%M%S%p", localtime)
-                subject = "移動偵測信通知信"
-                message="監視器在 " + check + "偵測到移動!! url:https://happy.shengda.ga/camera"
-                from_email=settings.EMAIL_HOST_USER
-                my_send_mail(subject, message,from_email, ['computer30422@gmail.com'])
-                cv2.imwrite("%s/output_%s.jpg" % (self.outputFolder, result1), img)
+                if self.mail_check:           
+                    self.send_mail(check,result1)
+                if self.scan_check: 
+                    self.photo_scan(img,result1)
+               
+    def send_mail(self,checkTime,result1):
+        #寄信
+        try:             
+           subject = "移動偵測信通知信"
+           url=settings.PRO_HOST + self.outputFolder + "/output_" + result1 + ".jpg"
+           message="監視器:" + str(self.title) + "在 " + checkTime + "偵測到移動!! url:"+url
+           from_email=settings.EMAIL_HOST_USER
+           my_send_mail(subject, message,from_email, ['computer30422@gmail.com'])
+           print("成功發送信件")
+        except:                 
+            print('發送信件失敗')
+    def photo_scan(self,img,result1):
+        #拍照
+        try:                
+           cv2.imwrite("%s/output_%s.jpg" % (self.outputFolder, result1), img)
+           print("成功儲存檔案")
+        except:                 
+            print('儲存檔案失敗')
     #時間相減
-    def timeSecond(self):
+    def timeSecond(self,Second):
         now_time=datetime.datetime.now()
         now_time=now_time.strftime("%Y-%m-%d %I:%M:%S")
         now_timer = datetime.datetime.strptime(now_time, r"%Y-%m-%d %I:%M:%S")
         end_time= self.min
         end_time = datetime.datetime.strptime(end_time, r"%Y-%m-%d %I:%M:%S")
         diff = now_timer - end_time
-        if(diff.total_seconds() > 60):
+        if(diff.total_seconds() > Second):
             self.min = now_time
             return now_time
         else:
@@ -162,14 +208,15 @@ class BaseCamera:
         獲取加工後的圖片，可以直接返回給前端顯示
         """
         img = self.queue.get()
-        if img is None:
+        if img is None or self.cam is None:
             return None
-        else:   
-            
+        else:          
             ret, frame = self.cam.read()
-            if(self.cam.isOpened()):
+            if(self.cam is not None and self.cam.isOpened()):
                 avg= cv2.blur(frame, (4, 4))
                 avg_float = np.float32(avg) 
+            else:
+                return None
             blur = cv2.blur(img, (4, 4))
 
             # 計算目前影格與平均影像的差異值
@@ -197,27 +244,28 @@ class BaseCamera:
                 # 偵測到物體，可以自己加上處理的程式碼在這裡...
                 hasMotion = True
                 # 計算等高線的外框範圍
-                (x, y, w, h) = cv2.boundingRect(c)
+                #(x, y, w, h) = cv2.boundingRect(c)
 
                 # 畫出外框
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             
             if hasMotion:
                 if admin is not None:
-                 self.send_mail(img)
+                 self.move_notice(img)
          # 儲存有變動的影像
             # 畫出等高線（除錯用）
             #cv2.drawContours(frame, cnts, -1, (0, 255, 255), 2)        
 
             # 更新平均影像
-            cv2.accumulateWeighted(blur, avg_float, 0.01)
-            avg = cv2.convertScaleAbs(avg_float)
+            #cv2.accumulateWeighted(blur, avg_float, 0.01)
+            #avg = cv2.convertScaleAbs(avg_float)
             #壓縮圖片，否則圖片過大，編碼效率慢，視頻延遲過高
             img = cv2.resize(img, (self.width, self.height), fx=0.25, fy=0.25)
             # 對圖片進行編碼
             ret, jpeg = cv2.imencode('.jpeg', img)
-            self.oldImg = img
             return jpeg.tobytes()
+            
+
 
 
 class CameraFactory:
@@ -254,5 +302,11 @@ class CameraFactory:
                 return None
         else:
             # 存在相機，直接返回
+            print("已存在")
+            camera_model = Camera.objects.get(camera_id=camera_id)
+            if camera.cam is None:
+                base_camera = BaseCamera(camera_model=camera_model,queue_image=queue_image)
+                return base_camera
+            camera.set_defalut(camera_model)
             return camera
        
--- camera/models.py
+++ camera/models.py
@@ -13,11 +13,20 @@ class Camera(models.Model):
     camera_id = models.TextField('攝影機編號', max_length=100,blank=True,unique=True)
     camera_url = models.TextField('攝影機位置', max_length=100,default='0',blank=True)
     title = models.CharField('標題', max_length=20,default='test')
+    width = models.IntegerField('螢幕寬度', default=1280,blank=True)
+    heigth = models.IntegerField('螢幕高度', default=720,blank=True)
+    fps = models.IntegerField('預設幀數', default=30,blank=True)
+    fast = models.IntegerField('移動偵測靈敏度', default=25,blank=True)
+    dday = models.IntegerField('固定刪除檔案時間(天數)', default=1,blank=True)
+    moveNotice = models.IntegerField('移動偵測通知時間(秒數)', default=60,blank=True)
+    mailCheck=models.BooleanField('是否移動發信',default=True)
+    scancheck=models.BooleanField('是否移動截圖',default=True)
+    isOpened=models.BooleanField('是否啟動',default=True)
     # Methods
     def camera_api(self):
         """Returns the URL to access a particular instance of MyModelName."""
         return int(self.camera_url)
-
+        # Methods
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
         return self.title
